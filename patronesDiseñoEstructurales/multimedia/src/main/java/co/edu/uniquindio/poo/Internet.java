48007d8472de0a03d75632b44537a27273a0186c
Soloquie <143025923+Soloquie@users.noreply.github.com>
2024-04-11 23:28:33 -0500

    Delete patronesDiseñoEstructurales/multimedia directory

--- patronesDiseñoEstructurales/multimedia/src/main/java/co/edu/uniquindio/poo/Internet.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public interface Internet {
-    void conectar(String sitioWeb);
-}
--- patronesDiseñoEstructurales/multimedia/src/main/java/co/edu/uniquindio/poo/InternetReal.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-// Implementación real de la interfaz Internet
-class InternetReal implements Internet {
-    @Override
-    public void conectar(String sitioWeb) {
-        System.out.println("Conectando a " + sitioWeb);
-    }
-}
